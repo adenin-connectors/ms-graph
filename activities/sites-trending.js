dfa1175f8b7c0f0592c8851d02c6e5f9fb4e829d
olliejm <olliejm@protonmail.com>
2020-04-03 16:54:50 +0100

    change avatar size

--- activities/sites-trending.js
+++ activities/sites-trending.js
@@ -26,7 +26,7 @@ module.exports = async (activity) => {
       // for Cisco and v1 cases we need to make the avatar app.adenin.com always.
       const plainTitle = helpers.stripSpecialChars(raw.resourceVisualization.title);
       const rawAvatar = $.avatarLink(plainTitle);
-      const avatar = `https://app.adenin.com/avatar${rawAvatar.substring(rawAvatar.lastIndexOf('/'), rawAvatar.length)}?color=1e4471&size=48&fontSize=64`;
+      const avatar = `https://app.adenin.com/avatar${rawAvatar.substring(rawAvatar.lastIndexOf('/'), rawAvatar.length)}?color=1e4471&size=52&fontSize=64`;
 
       activity.Response.Data.items.push({
         id: raw.id,
